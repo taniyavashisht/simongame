5ebd94f2867a10b0170c9442e098b9d0f905eb43
Taniya Vashisht <112457448+taniyavashisht@users.noreply.github.com>
2024-07-13 15:33:04 +0530

    Update game.js

--- game.js
+++ game.js
@@ -13,17 +13,6 @@ $(document).keypress(function(){
     }
 });
 
-$(document).click(function(){
-    if(!started){
-        $("#level-title").text("Level "+level);
-        nextSequence();
-        started=true;
-    }
-});
-
-
-
-
 $(".btn").click(function(){
     var userChosenColour=$(this).attr("id");
     userClickedPattern.push(userChosenColour);
